c698ccff94a2068ab815e744d66b2ccd3e56f8a7
Tonjar <Tonjar_tyz@163.com>
2019-11-07 10:18:44 +0800

    Add files via upload

--- task/list.js
+++ task/list.js
@@ -4,9 +4,9 @@ toprightcol=mainpage.appendChild(document.createElement('div'))
 toprightcol.setAttribute('class','toprightcol')
 bottomcol=mainpage.appendChild(document.createElement('div'))
 bottomcol.setAttribute('class','bottomcol')
-
-
-
+if(IsPC()==false){
+    mainpage.setAttribute("class","phone_mainpage mainpage")
+}
 s=body.appendChild(document.createElement('script'))
 s.setAttribute('src','week.js')
 s=body.appendChild(document.createElement('script'))
