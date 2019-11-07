4862b3efe297c432b6a9b67643a5cbde68b95410
Tonjar <Tonjar_tyz@163.com>
2019-11-07 10:18:07 +0800

    Add files via upload

--- com/notmain.js
+++ com/notmain.js
@@ -181,3 +181,18 @@ function dragFunc(Drag) {
         this.style.cursor = "default";
     };
 };
+
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
